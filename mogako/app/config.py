05a7525f3caae5088fb512cb6e29ee6a20ea4a1a
kuri-thevc <60633084+ku-ri@users.noreply.github.com>
2022-09-25 17:09:18 +0900

    fix : config.py

--- mogako/app/config.py
+++ mogako/app/config.py
@@ -3,37 +3,41 @@ get_settings gets called for each request.
  lru_cache to cache the settings so get_settings is only called once.
 lru_cache
 https://docs.python.org/3/library/functools.html#functools.lru_cache
-
 """
 import logging
-from os import path, environ
+import os
 from dataclasses import dataclass
 
+from dotenv import load_dotenv
 
 log = logging.getLogger("uvicorn")
-
-base_dir = path.dirname(path.dirname(path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+load_dotenv(os.path.join(BASE_DIR, "..", ".env"))
 
 
 @dataclass
 class Config:
-    BASE_DIR = base_dir
+    BASE_DIR = BASE_DIR
     DB_POOL_RECYCLE: int = 900
     DB_ECHO: bool = True
+    DB_USER: str = os.getenv("DB_USER")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD")
+    DB_HOST: str = os.getenv("DB_HOST")
+    DB_PORT: str = os.getenv("DB_PORT")
+    DB_NAME: str = os.getenv("DB_NAME")
+    DB_URL: str = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4"
 
 
 @dataclass
 class LocalConfig(Config):
     PROJ_RELOAD: bool = True
-    DB_URL: str = "mysql+pymysql://root:root@127.0.0.1:3307/mogako?charset=utf8mb4"
 
 
 @dataclass
 class DevConfig(Config):
     PROJ_RELOAD: bool = False
-    DB_URL: str = "mysql+pymysql://db_manager:dpvmfoq1A@db-bj72i.pub-cdb.ntruss.com/mogagko-db?charset=utf8mb4"
 
 
 def conf():
     config = dict(dev=DevConfig(), local=LocalConfig())
-    return config.get(environ.get("ENVIRONMENT", "local"))
+    return config.get(os.getenv("ENVIRONMENT", "local"))
