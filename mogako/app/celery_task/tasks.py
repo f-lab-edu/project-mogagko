12bca9eb62bf0568acdae8af07a6fc08c17885fe
최신혁 <68194553+shinhyuk-choi@users.noreply.github.com>
2022-10-23 21:20:56 +0900

    feat: insert to db crawled cafe (#26)
    * feat: insert to db crawled cafe
    
    * add: severals
    
    Co-authored-by: shchoi <shchoi@payhere.in>

--- mogako/app/celery_task/tasks.py
+++ mogako/app/celery_task/tasks.py
@@ -1,12 +1,16 @@
 import asyncio
 import os
+import uuid
 from typing import List, Dict, Any, Optional
 
 import aiohttp
 from dotenv import load_dotenv
 
 from celery import Celery
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+from mogako.db.model.cafe import Cafe
 
 celery = Celery(
     __name__, broker="redis://127.0.0.1:6379/0", backend="redis://127.0.0.1:6379/0"
@@ -50,24 +54,49 @@ async def afetch_url(session: aiohttp.ClientSession, url: str, responses):
 
 
 @celery.task(name="bulk_insert_cafes")
-def bulk_insert_cafes(cafe_jsons: List[Dict[str, Any]]):
+def bulk_insert_cafes(cafe_jsons: Dict[str, Any]):
+    kakao_cafe_ids = cafe_jsons.keys()
+    DB_USER = os.environ.get("DB_USER")
+    DB_PASSWORD = os.environ.get("DB_PASSWORD")
+    DB_HOST = os.environ.get("DB_HOST")
+    DB_PORT = os.environ.get("DB_PORT")
+    DB_NAME = os.environ.get("DB_NAME")
+
+    engine = create_engine(
+        f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4",
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    already_exist_kakao_cafe_ids = (
+        session.query(Cafe.kakao_cafe_id)
+        .filter(Cafe.kakao_cafe_id.in_(kakao_cafe_ids))
+        .all()
+    )
     print("bulk_insert_cafes")
-    with open("./test.txt", "a+") as file:
-        file.write(f"length: {len(cafe_jsons)}\n")
-        file.write(f"cafes: {cafe_jsons}\n")
+    already_exist_kakao_cafe_ids = [
+        str(kakao_cafe_id[0]) for kakao_cafe_id in already_exist_kakao_cafe_ids
+    ]
+    objects = [
+        Cafe(external_key=str(uuid.uuid4()), **cafe_json)
+        for cafe_json in cafe_jsons.values()
+        if cafe_json["kakao_cafe_id"] not in already_exist_kakao_cafe_ids
+    ]
+    session.bulk_save_objects(objects)
+    session.commit()
+    session.close()
 
 
 @celery.task(name="parse_responses")
 def parse_responses(responses: List[Dict[str, Any]]):
     print("parse_responses")
-    cafe_jsons = []
+    cafe_jsons = {}
     next_urls = []
     for response in responses:
         next_url = _parse_next_url(response=response)
         if next_url:
             next_urls.append(next_url)
         cafe_jsons_of_response = _parse_cafe_jsons(response=response)
-        cafe_jsons.extend(cafe_jsons_of_response)
+        cafe_jsons.update(cafe_jsons_of_response)
 
     # Publish urls
     if next_urls:
@@ -89,17 +118,16 @@ def _parse_next_url(response: Dict[str, Any]) -> Optional[str]:
     return None
 
 
-def _parse_cafe_jsons(response: Dict[str, Any]) -> List[Dict[str, Any]]:
-    cafe_jsons = []
+def _parse_cafe_jsons(response: Dict[str, Any]) -> Dict[str, Any]:
+    cafe_jsons = {}
     try:
         for cafe in response["documents"]:
-            cafe_jsons.append(
-                {
-                    "name": cafe["place_name"],
-                    "address": cafe["address_name"],
-                }
-            )
+            cafe_jsons[cafe["id"]] = {
+                "name": cafe["place_name"],
+                "address": cafe["address_name"],
+                "kakao_cafe_id": cafe["id"],
+            }
     except KeyError:
         print(f"KeyError: {response}")
-        return []
+        return {}
     return cafe_jsons
